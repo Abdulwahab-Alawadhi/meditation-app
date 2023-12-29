c90f6c050a227151ba9c72d16b6ef2b7d3abd331
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2023-12-29 11:39:40 +0300

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:meditation_app/pages/home_page.dart';
 import 'package:meditation_app/pages/signin_page.dart';
 import 'package:meditation_app/pages/signup_page.dart';
 
@@ -21,6 +22,13 @@ final GoRouter _router = GoRouter(
       name: "signup",
       builder: (BuildContext context, GoRouterState state) {
         return SignupPage();
+      },
+    ),
+    GoRoute(
+      path: '/home',
+      name: "home",
+      builder: (BuildContext context, GoRouterState state) {
+        return HomePage();
       },
     ),
   ],
